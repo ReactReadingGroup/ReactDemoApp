a49ea42fb9a4eea4cd107fdd6b152dd8756da618
ReactReadingGroup <156299419+ReactReadingGroup@users.noreply.github.com>
2024-02-01 13:20:49 +0530

    Add files via upload
    syncing practice project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Components/Nav/Nav';
+import Footer from './Components/Footer/Footer';
+import Products from './Components/Products/Products';
+
+function App() {
+  return (
+      <>
+        <Nav />
+        <Products/>
+        <Footer />
+      </>
+  );
+}
+
+export default App;
