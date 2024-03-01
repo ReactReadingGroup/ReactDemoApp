37870326d19d68b96b8ff9e2720341cd353615b9
whoakshayparihar <114063298+Akshaysinghparihar@users.noreply.github.com>
2024-03-01 17:06:25 +0530

    Add files via upload
    updated app.js file for hooks

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Nav from './Components/Nav/Nav';
 import Footer from './Components/Footer/Footer';
 import Products from './Components/Products/Products';
 import { ProductObj } from "./Components/Products/ProductsObj";
+import React, { useState } from "react";
 
 function App() {
+  const[myCart, setMyCart]= useState([]);
   return (
       <>
-        <Nav />
-        <Products productObj={ProductObj}/>
+        <Nav myCart= {myCart}/>
+        <Products productObj={ProductObj} cartValues = {setMyCart}/>
         <Footer />
       </>
   );
