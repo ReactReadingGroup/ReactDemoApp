3982a2d5870494631f352b7470edc9f7b91ebdaf
Pransu Kushwaha <Pransu.Kushwaha@genmills.com>
2024-03-20 15:20:21 +0530

    added session 4 changes

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-function Nav(){
+function Nav(props){
+    const cartCount = props.myCart.length;
+    let cartCountClass = cartCount == 0? "":"add-card";
+    console.log(cartCount);
+    console.log(cartCountClass);
     return(
         <>
             <nav className="py-2 bg-body-tertiary border-bottom">
@@ -16,7 +20,7 @@ function Nav(){
                     <ul className="nav">
                         <li className="nav-item"><a href="#" className="nav-link link-body-emphasis px-2">Login</a></li>
                         <li className="nav-item"><a href="#" className="nav-link link-body-emphasis px-2">Sign up</a></li>
-                        <li className="nav-item add-card"><a href="#" className="nav-link link-body-emphasis px-2"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart3" viewBox="0 0 16 16">
+                        <li className={`nav-item ${cartCountClass}`} data-content ={cartCount}><a href="#" className="nav-link link-body-emphasis px-2"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart3" viewBox="0 0 16 16">
                             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
                         </svg></a></li>
                     </ul>
